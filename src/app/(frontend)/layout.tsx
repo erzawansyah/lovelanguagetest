8c27726832effc3f3b83a41d6637bf50544818cd
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2023-11-04 13:06:09 +0700

    add clarity code

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,12 +2,13 @@ import Header from '@/app/(frontend)/_components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Love Language Test App',
-  description: 'Powered by PT Literasi Kreatif Indonesia'
+  description: 'Powered by PT Literasi Kreatif Indonesia',
 }
 const RootLayout = ({
   children,
@@ -18,6 +19,7 @@ const RootLayout = ({
 
   return (
     <html lang="en">
+      <MsClarityCode />
       <body className={`${inter.className} flex flex-col h-screen`}>
         <Header />
         <main className='relative flex-grow overflow-auto'>
@@ -30,3 +32,20 @@ const RootLayout = ({
 }
 
 export default RootLayout
+
+
+const MsClarityCode = () => {
+  return <>
+    <Script
+      id="ms-clarity-code"
+      strategy='afterInteractive'
+
+    >
+      {`(function (c, l, a, r, i, t, y) {
+        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", "jl38fg70wk")`}
+    </Script>
+  </>
+}
